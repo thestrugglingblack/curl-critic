d8cdd846017d15ec10a9933a6db3292a266427f0
ZuriHunter <zuri.hunter@mapbox.com>
2018-04-27 00:28:16 -0400

    First step of setting up route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
-import  NavBar from '../components/navbar/navbar'
+import  NavBar from '../components/navbar/navbar';
+import Main from '../components/main/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <NavBar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Main/>
       </div>
     );
   }
